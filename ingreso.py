6f36bb9a388156f73150a4018362146899b331dc
Johan-Herrera <64096529+Johan-Herrera@users.noreply.github.com>
2020-12-02 18:30:52 -0500

    Agrego documentacion del archivo ingreso.py

--- ingreso.py
+++ ingreso.py
@@ -8,17 +8,17 @@ datos=cgi.FieldStorage()
 print('Content-Type: text/html')
 print('')
 try:
-  idc=None
-  datos=cgi.FieldStorage()
-  cnx = mysql.connector.connect(user='johan', password = 'johan0123', database='ejemplo', host='127.0.0.1')
-  cursor=cnx.cursor()
-  id = datos.getvalue('id1')
+  idc=None  #Se le asigna None a la variable que guaradara la Identificacion del cliente
+  datos=cgi.FieldStorage()  #Se extraen los datos que se encontraron en el formulario del HTML
+  cnx = mysql.connector.connect(user='johan', password = 'johan0123', database='ejemplo', host='127.0.0.1')  #Se hace conexion con la base de datos
+  cursor=cnx.cursor()  #Se crea el objeto que mandara los datos a la base de datos
+  id = datos.getvalue('id1') 
   clave = datos.getvalue('clave1')
   comillas = "'"
-  cursor.execute( "select * from cliente where ID_Cliente="+id+" and clave="+comillas+clave+comillas)
-  for ID_Cliente in cursor.fetchall() :
+  cursor.execute( "select * from cliente where ID_Cliente="+id+" and clave="+comillas+clave+comillas)  #Se hace la busqueda con codigo SQL en la base de datos
+  for ID_Cliente in cursor.fetchall() :    #Se extraen los datos resultantes de l busqueda hecha por la base de datos y se comparan con el ID que halla ingresado el cliente
     idc=ID_Cliente
-  if(idc!=None):
+  if(idc!=None):  #Se hace el render de una pagina con los datos de la ID buscada 
          with open('vistas/interfazCliente.html' ,'r') as f:
                  doc = f.read()
                  template = Template(doc)
@@ -26,8 +26,8 @@ try:
                  print(page)
   else:
          print("Has ingresado una Identificacion Inexistente")
-  cnx.close()
-except mysql.connector.Error as err:
+  cnx.close()  #Se finaliza la conexion con la base de datos
+except mysql.connector.Error as err:  #Se hace el manejo de errores basico
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with your user name or password")
   elif err.errno == errorcode.ER_BAD_DB_ERROR:
